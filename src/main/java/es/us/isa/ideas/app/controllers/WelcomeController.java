9728084a4c235397f97257de2c911a650fd08854
Dany Alonso <danyal@MacBook-Pro-de-Dany.local>
2016-01-28 14:37:21 +0100

    Context /IDEAS changed for / Root redirect to /welcome (welcome page)

--- src/main/java/es/us/isa/ideas/app/controllers/WelcomeController.java
+++ src/main/java/es/us/isa/ideas/app/controllers/WelcomeController.java
@@ -1,5 +1,8 @@
 package es.us.isa.ideas.app.controllers;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,20 +12,31 @@ import org.springframework.web.servlet.ModelAndView;
 public class WelcomeController extends AbstractController {
 
 	// Constructors -----------------------------------------------------------
-
+	
 	public WelcomeController() {
 		super();
 	}
+		
+	// Index ------------------------------------------------------------------		
 
-	// Index ------------------------------------------------------------------
-
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView home() {
-		return index();
-	}
-
+        @RequestMapping(value = "/", method = RequestMethod.GET)
+        public ModelAndView home(){
+            return index();
+        }
+        
+        
 	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
 	public ModelAndView index() {
-		return new ModelAndView("redirect:/app/editor");
+		ModelAndView result;
+		SimpleDateFormat formatter;
+		String moment;
+		
+		formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		moment = formatter.format(new Date());
+				
+		result = new ModelAndView("welcome");		
+//		result = new ModelAndView("redirect:/app/editor");	
+
+		return result;
 	}
 }
